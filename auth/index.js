d1977499b4490721f8dd0b6cc3c67c0ae36baf12
WILL MEIER <willmeierart@gmail.com>
2017-06-22 13:31:45 -0600

    full auth functionality

--- auth/index.js
+++ auth/index.js
@@ -3,75 +3,87 @@ const bcrypt = require('bcrypt')
 const router = express.Router()
 const User = require('../db/queries')
 
-router.get('/', (req,res)=>{
-    res.json({
-        message: '✌️'
-    })
+router.get('/', (req, res) => {
+  res.json({
+    message: '✌️'
+  })
 })
 
-function validUser(user){
-    const validEmail = typeof user.email == 'string' && user.email.trim() != ''
-    const validPassword = typeof user.password == 'string' && user.password.trim() != '' && user.password.trim().length >= 4
-    return validEmail && validPassword
+function validUser(user) {
+  const validEmail = typeof user.email == 'string' && user.email.trim() != ''
+  const validPassword = typeof user.password == 'string' && user.password.trim() != '' && user.password.trim().length >= 4
+  return validEmail && validPassword
 }
 
-router.post('/signup', (req, res, next)=>{
-    if (validUser(req.body)){
-        User.getUserByEmail(req.body.email)
-        .then(user=>{
-            if(!user){
-                bcrypt.hash(req.body.password, 10)
-                .then(hash=>{
-                    const user = req.body
-                    user.password = hash
-                    User.createUser(user).then(id=>{
-                        //redirect
-                        res.json({
-                            id,
-                            message: '☑︎'
-                        })
-                    })
+router.post('/signup', (req, res, next) => {
+  if (validUser(req.body)) {
+    User.getUserByEmail(req.body.email)
+      .then(user => {
+        if (!user) {
+          bcrypt.hash(req.body.password, 10)
+            .then(hash => {
+              const user = req.body
+              user.password = hash
+              User.createUser(user).then(id => {
+                setUserIdCookie(req, res, id)
+                //redirect
+                res.json({
+                  id,
+                  message: '☑︎'
                 })
-            }else{
-                next(new Error('Email in use'))
-            }
-        })
-    } else {
-        next(new Error('Invalid User'))
-    }
+              })
+            })
+        } else {
+          next(new Error('Email in use'))
+        }
+      })
+  } else {
+    next(new Error('Invalid User'))
+  }
 })
 
-router.post('/login', (req,res, next)=>{
-    console.log(req.body)
-    if(validUser(req.body)){
-        //check if in db
-        User.getUserByEmail(req.body.email).then(user=>{
-            console.log('user', user)
-            if(user){
-                //compare password with hashed password
-                bcrypt.compare(req.body.password, user.password).then(result=>{
-                    if(result){
-                        //if passwords matched
-                        //set-cookie header
-                        const isSecure = req.app.get('env') != 'development'
-                        res.cookie('user_id', user.id, {
-                            httpOnly: true,
-                            secure: isSecure,
-                            signed: true
-                        })
-                        res.json({
-                            id: user.id,
-                            message: 'Logged in! 🔓'
-                        })
-                    }
-                })
-            } else {
-                next(new Error('Invalid login'))
-            }
+function setUserIdCookie(req, res, id) {
+  const isSecure = req.app.get('env') != 'development'
+  res.cookie('user_id', id, {
+    httpOnly: true,
+    secure: isSecure,
+    signed: true
+  })
+  res.json({
+    message: '🔓'
+  })
+}
+
+router.post('/login', (req, res, next) => {
+  console.log(req.body)
+  if (validUser(req.body)) {
+    //check if in db
+    User.getUserByEmail(req.body.email).then(user => {
+      console.log('user', user)
+      if (user) {
+        //compare password with hashed password
+        bcrypt.compare(req.body.password, user.password).then(result => {
+          if (result) {
+            //if passwords matched
+            //set-cookie header
+            setUserIdCookie(req, res, user.id)
+
+          }
         })
-    } else {
+      } else {
         next(new Error('Invalid login'))
-    }
+      }
+    })
+  } else {
+    next(new Error('Invalid login'))
+  }
+})
+
+router.get('/logout', (req, res) => {
+  res.clearCookie('user_id')
+  res.json({
+    message: '🔒'
+  })
 })
 
 module.exports = router
